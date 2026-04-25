fb68886446ae0a5e64d8672f0e0cca9903516720
ploharkar-dev <ploharkar@gmail.com>
2026-04-25 16:29:23 +0530

    Initial Commit

---
+++ src/test/java/com/prl/smartexpensetracker/SmartexpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prl.smartexpensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartexpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
